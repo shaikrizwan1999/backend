736cc96792ff2a13dc43711149a6d739359b0cdf
Shaik Rizwan Basha <89855963+shaikrizwan1999@users.noreply.github.com>
2022-01-18 16:02:02 +0530

    backend
    Controllers

---
+++ Locations.js
@@ -0,0 +1,19 @@
+const Location = require('../Models/Locations');
+
+
+// export the below controller functionality
+
+// get all the locations
+exports.getAllLocations = (req, res) => {
+    Location.find().then(result => {
+        res.status(200).json({
+            message: "Locations fetched",
+            locations: result
+        });
+    }).catch(error => {
+        res.status(500).json({
+            message: "Error in Database",
+            error: error
+        });
+    });
+}
